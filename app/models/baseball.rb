66f51431e5a7dcf5a99bdbd4979d9bb3113ae7a2
JungYuCheol <cokr3430@gmail.com>
2016-02-01 10:42:43 +0000

    helper

--- app/models/baseball.rb
+++ app/models/baseball.rb
@@ -10,6 +10,34 @@ class Baseball < ActiveRecord::Base
     return self.last.number
   end
   
-  def check_correct(input)
+  def self.check_this(input)
+    current_number = self.get_current_number
+    answer_array = current_number.to_s.split("")
+    input_array = input.to_s.split("")
+    strike = 0
+    ball = 0
+    out = true
+    
+    for i in 0..2
+      if answer_array.include? input_array[i]
+        if answer_array[i] == input_array[i]
+          strike = strike + 1
+          out = false
+        else
+          ball = ball + 1
+          out = false
+        end
+      end
+    end
+    
+    if out
+      return "Out!!"
+    else
+      if strike == 3
+        return "You win!"
+      else
+        return "#{strike} strikes and #{ball} ball"
+      end
+    end
   end
 end
